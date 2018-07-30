9af5853ec183fea508f412b8e6f2c69311692cbd
Claudio <claudiocamba1@gmail.com>
2018-07-30 19:27:51 +0100

    portfolio files

---
+++ assets/js/portfolio.js
@@ -0,0 +1,12 @@
+$('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
+var offset = 100;
+    $('.navbar li a').click(function(event) {
+        event.preventDefault();
+        $($(this).attr('href'))[0].scrollIntoView();
+        scrollBy(0, -offset);
+    });
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();
+});
